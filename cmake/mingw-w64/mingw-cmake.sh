156054eef4f4e8ef403a6835f39d3481ead3032c
Martchus <martchus@gmx.net>
2016-09-11 23:36:59 +0200

    Add option in mingw-w64-cmake to prevent frocing shared libs

--- cmake/mingw-w64/mingw-cmake.sh
+++ cmake/mingw-w64/mingw-cmake.sh
@@ -7,16 +7,20 @@ mingw_flags="${CUSTOM_MINGW_FLAGS:--O2 -g -pipe -Wall -Wp,-D_FORTIFY_SOURCE=2 -f
 export CFLAGS="$mingw_flags $CFLAGS"
 export CXXFLAGS="$mingw_flags $CXXFLAGS"
 
-PATH=${mingw_prefix}/bin:$PATH cmake \
-    -DCMAKE_INSTALL_PREFIX:PATH=${mingw_prefix} \
-    -DCMAKE_INSTALL_LIBDIR:PATH=${mingw_prefix}/lib \
-    -DINCLUDE_INSTALL_DIR:PATH=${mingw_prefix}/include \
-    -DCMAKE_C_IMPLICIT_INCLUDE_DIRECTORIES:PATH=${mingw_prefix}/include \
-    -DCMAKE_CXX_IMPLICIT_INCLUDE_DIRECTORIES:PATH=${mingw_prefix}/include \
-    -DLIB_INSTALL_DIR:PATH=${mingw_prefix}/lib \
-    -DSYSCONF_INSTALL_DIR:PATH=${mingw_prefix}/etc \
-    -DSHARE_INSTALL_DIR:PATH=${mingw_prefix}/share \
-    -DBUILD_SHARED_LIBS:BOOL=ON \
-    -DCMAKE_TOOLCHAIN_FILE=/usr/share/mingw/toolchain-@TRIPLE@.cmake \
-    -DCMAKE_CROSSCOMPILING_EMULATOR=/usr/bin/@TRIPLE@-wine \
-    "$@"
+additional_args=" \
+  -DCMAKE_INSTALL_PREFIX:PATH=${mingw_prefix} \
+  -DCMAKE_INSTALL_LIBDIR:PATH=${mingw_prefix}/lib \
+  -DINCLUDE_INSTALL_DIR:PATH=${mingw_prefix}/include \
+  -DCMAKE_C_IMPLICIT_INCLUDE_DIRECTORIES:PATH=${mingw_prefix}/include \
+  -DCMAKE_CXX_IMPLICIT_INCLUDE_DIRECTORIES:PATH=${mingw_prefix}/include \
+  -DLIB_INSTALL_DIR:PATH=${mingw_prefix}/lib \
+  -DSYSCONF_INSTALL_DIR:PATH=${mingw_prefix}/etc \
+  -DSHARE_INSTALL_DIR:PATH=${mingw_prefix}/share \
+  -DCMAKE_TOOLCHAIN_FILE=/usr/share/mingw/toolchain-@TRIPLE@.cmake \
+  -DCMAKE_CROSSCOMPILING_EMULATOR=/usr/bin/@TRIPLE@-wine"
+
+[[ ! $PREVENT_FORCING_SHARED_LIBS ]] &&
+  additional_args+=' -DBUILD_SHARED_LIBS:BOOL=ON' ||
+  additional_args+=' -DBUILD_SHARED_LIBS:BOOL=OFF'
+
+PATH=${mingw_prefix}/bin:$PATH cmake $additional_args "$@"

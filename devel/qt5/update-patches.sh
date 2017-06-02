98abd6df3be8be36b9104b80bb3202671dbd12b5
Martchus <martchus@gmx.net>
2017-06-02 18:20:04 +0200

    Update mingw-w64-qt5 packages to 5.9.0

--- devel/qt5/update-patches.sh
+++ devel/qt5/update-patches.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 
-# Copies patches from QTBASE_GIT_WORKING_DIR to default
-# variant of mingw-w64-qt5-base and outputs altered source and
+# Copies patches from QT_GIT_REPOS_DIR to default
+# variant of specified repo and outputs altered source and
 # md5sums arrays
 
 #set -euxo pipefail
@@ -9,7 +9,7 @@ set -e # abort on first error
 shopt -s nullglob
 
 if ! [[ $1 ]]; then
-    echo 'No Qt repo specified.'
+    echo 'No Qt repo specified - must be specified like eg. base or multimedia.'
     exit -1
 fi
 
@@ -17,6 +17,15 @@ pkg="qt5-$1"
 repo="qt$1"
 dest="${DEFAULT_PKGBUILDS_DIR}/${pkg}/mingw-w64"
 wd="${QT_GIT_REPOS_DIR}/${repo}"
+
+if ! [[ -d $wd ]]; then
+    echo "\$QT_GIT_REPOS_DIR/$repo is no directory."
+    exit -2
+fi
+if ! [[ -d $dest ]]; then
+    echo "\$DEFAULT_PKGBUILDS_DIR/$pkg/mingw-w64 is no directory."
+    exit -3
+fi
 
 source "$dest/PKGBUILD"
 
@@ -26,7 +35,7 @@ file_index=0
 for source in "${source[@]}"; do
 	[ "${source: -6}" != .patch ] && \
 		new_sources+=("$source") \
-		new_md5sums+=("${md5sums[$file_index]}")
+		new_md5sums+=("${sha256sums[$file_index]}")
 	file_index=$((file_index + 1))
 done
 
@@ -41,13 +50,20 @@ done
 
 pushd "$wd" > /dev/null
 git checkout "${pkgver}-mingw-w64"
-git format-patch "${pkgver}" --output-directory "$dest"
+remote=
+for maybe_remote in 'martchus' 'upstream'; do
+	if git remote get-url $maybe_remote; then
+		remote=$maybe_remote
+		break
+	fi
+done
+git format-patch "${remote}/${pkgver}" --output-directory "$dest"
 popd > /dev/null
 
 new_patches=("$dest"/*.patch)
 for patch in "${new_patches[@]}"; do
 	new_sources+=("$patch")
-	sum=$(md5sum "$patch")
+	sum=$(sha256sum "$patch")
 	new_md5sums+=(${sum%% *})
 done
 
@@ -58,9 +74,9 @@ for source in "${new_sources[@]:1}"; do
 done
 echo ')'
 
-echo -n "md5sums=('${new_md5sums[0]}'"
+echo -n "sha256sums=('${new_md5sums[0]}'"
 for sum in "${new_md5sums[@]:1}"; do
 	echo
-	echo -n "         '${sum}'"
+	echo -n "            '${sum}'"
 done
 echo ')'

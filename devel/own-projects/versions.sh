186338332630328a8ad0d00510240b83f149f7be
Marius Kittler <mkittler@suse.de>
2018-05-17 19:20:21 +0200

    Update own projects

--- devel/own-projects/versions.sh
+++ devel/own-projects/versions.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 declare -A versions=(
-    [c++utilities]=4.14.0
+    [c++utilities]=4.14.1
     [qtutilities]=5.10.0
-    [tagparser]=7.0.2
-    [tageditor]=3.0.1
+    [tagparser]=7.0.3
+    [tageditor]=3.1.0
     [passwordfile]=3.1.4
     [passwordmanager]=3.1.5
-    [syncthingtray]=0.8.0
+    [syncthingtray]=0.8.1
     [videodownloader]=1.3.3
     [reflective-rapidjson]=0.0.5
     [blackwidowcontrol]=1.0.1

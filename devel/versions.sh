e9665de6744d0acaf9fe57229cea4ee4fd5e12eb
Martchus <martchus@gmx.net>
2019-02-18 00:43:34 +0100

    Update own projects

--- devel/versions.sh
+++ devel/versions.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 declare -A versions=(
-    [c++utilities]=4.16.0
-    [qtutilities]=5.12.1
-    [tagparser]=8.1.0
-    [tageditor]=3.1.3
-    [passwordfile]=4.0.0
-    [passwordmanager]=4.0.0
     [syncthingtray]=0.8.4
+    [c++utilities]=4.17.0
+    [qtutilities]=5.12.2
+    [tagparser]=8.2.0
+    [tageditor]=3.2.0
+    [passwordfile]=4.0.1
+    [passwordmanager]=4.0.1
     [videodownloader]=1.3.3
     [reflective-rapidjson]=0.0.6
     [blackwidowcontrol]=1.0.1
     [dbus-soundrecorder]=1.2.2
-    [qt5]=5.12.0
+    [qt5]=5.12.1
 )
 declare -A github_names=(
     [c++utilities]=cpp-utilities

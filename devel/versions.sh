2bcb83996ee3e8e85009b0c4011c995a52f7dd3e
Martchus <martchus@gmx.net>
2019-04-21 22:30:55 +0200

    Update own projects

--- devel/versions.sh
+++ devel/versions.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 declare -A versions=(
-    [syncthingtray]=0.9.0
-    [c++utilities]=4.17.0
+    [syncthingtray]=0.9.1
+    [c++utilities]=4.17.1
     [qtutilities]=5.13.0
-    [tagparser]=8.2.0
-    [tageditor]=3.2.0
+    [tagparser]=8.3.0
+    [tageditor]=3.2.1
     [passwordfile]=4.0.1
-    [passwordmanager]=4.0.1
+    [passwordmanager]=4.0.2
     [videodownloader]=1.3.3
-    [reflective-rapidjson]=0.0.7
+    [reflective-rapidjson]=0.0.8
     [blackwidowcontrol]=1.0.1
     [dbus-soundrecorder]=1.2.2
     [qt5]=5.12.3

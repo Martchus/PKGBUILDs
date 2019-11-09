d7752b8687c1540062abdf35176f31ebed51f2bd
Martchus <martchus@gmx.net>
2019-11-09 03:03:56 +0100

    Update mingw-w64-qt5-* to 5.13.2

--- devel/versions.sh
+++ devel/versions.sh
@@ -12,7 +12,7 @@ declare -A versions=(
     [blackwidowcontrol]=1.0.1
     [dbus-soundrecorder]=1.2.3
     [geocoordinatecalculator]=1.1.3
-    [qt5]=5.13.1
+    [qt5]=5.13.2
 )
 declare -A github_names=(
     [c++utilities]=cpp-utilities

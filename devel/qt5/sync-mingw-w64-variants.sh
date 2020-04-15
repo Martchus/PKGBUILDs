24fde8431264ff759d8942800f917a48710df479
Martchus <martchus@gmx.net>
2020-04-15 11:18:19 +0200

    Fix problems with mingw-w64-qt5 packages
    * https://aur.archlinux.org/packages/mingw-w64-qt5-virtualkeyboard/#comment-739521
    * https://aur.archlinux.org/packages/mingw-w64-qt5-base-dynamic/#comment-739527

--- devel/qt5/sync-mingw-w64-variants.sh
+++ devel/qt5/sync-mingw-w64-variants.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/bash -e
-"$(dirname "$0")/../sync-variants.sh" qt5-base mingw-w64 mingw-w64-{static,opengl,angle}
+"$(dirname "$0")/../sync-variants.sh" qt5-base mingw-w64 mingw-w64-{static,opengl,angle,dynamic}

567f591ea0813ac218cebf13e71e4e42393eaae6
Martchus <martchus@gmx.net>
2022-05-01 13:55:22 +0200

    Update freetype2 to 2.12.1

--- freetype2/sync-variants.sh
+++ freetype2/sync-variants.sh
@@ -3,6 +3,7 @@
 # Syncs the different variants of mingw-w64-qt5-base
 
 set -e # abort on first error
+shopt -s extglob
 master="${1:-mingw-w64}"
 
 if [ $# -gt 1 ]; then
@@ -17,7 +18,8 @@ fi
 for dir in *; do
   if [[ $dir != $master ]] && [[ -d $dir ]] && [[ $dir =~ mingw-w64-.* ]]; then
     rm "$dir/"* # clean first (files might have been remove in master)
-    cp "$master/"* "$dir"
+    cp "$master/"PKGBUILD "$dir"
+    ln -rs "$master/"!(PKGBUILD) "$dir"
     sed -e '/pkgname=mingw-w64-freetype2/{c\pkgname=mingw-w64-freetype2'${dir#mingw-w64} -e ';d}' "$master/PKGBUILD" > "$dir/PKGBUILD"
   fi
 done

a10fd8c94b6bedbe2c917826993b8de6b1d0b95f
Martchus <martchus@gmx.net>
2022-05-16 20:00:12 +0200

    Rebase Qt 5 packages against 5.15.4

---
+++ devel/generator/gen-kde-fork-revisions.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e # abort on first error
+shopt -s nullglob
+bindir=$(dirname "$0")
+source "$bindir/../versions.sh"
+expected_version=${versions[qt5]}.*
+
+arch_pkgbuilds=$1
+[[ $arch_pkgbuilds ]] && cd "$arch_pkgbuilds"
+
+for pkgbuild in qt5-*/trunk/PKGBUILD ; do
+    source "$pkgbuild"
+    if [[ $pkgname != qt5-doc ]] && [[ $pkgver =~ $expected_version ]] && [[ $_commit ]]; then
+        echo "${pkgname##qt5-} => [${pkgver##*+r}, '$_commit']",
+    fi
+done
+

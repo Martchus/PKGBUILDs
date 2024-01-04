084636ac44f27140ed8d22ded9dc2b0536cea50e
Martchus <martchus@gmx.net>
2024-01-04 16:09:11 +0100

    Update mingw-w64-qt5 packages to 5.15.12

--- devel/versions.sh
+++ devel/versions.sh
@@ -13,7 +13,7 @@ declare -A versions=(
     [blackwidowcontrol]=1.1.0
     [dbus-soundrecorder]=1.2.3
     [geocoordinatecalculator]=1.1.3
-    [qt5]=5.15.11
+    [qt5]=5.15.12
     [qt6]=6.6.1
 )
 declare -A github_names=(

104c0577376eb3d1b457c5868a44e0b6e9e1fd58
Martchus <martchus@gmx.net>
2024-02-15 16:48:07 +0100

    Update Qt 6 packages to 6.6.2

--- devel/versions.sh
+++ devel/versions.sh
@@ -14,7 +14,7 @@ declare -A versions=(
     [dbus-soundrecorder]=1.2.3
     [geocoordinatecalculator]=1.1.3
     [qt5]=5.15.12
-    [qt6]=6.6.1
+    [qt6]=6.6.2
 )
 declare -A github_names=(
     [c++utilities]=cpp-utilities

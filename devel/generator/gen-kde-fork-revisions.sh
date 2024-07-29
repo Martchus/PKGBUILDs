770f8c0b1f890ca3e26c1deffabd4609cc86422f
Martchus <martchus@gmx.net>
2024-07-30 00:23:28 +0200

    Update mingw-w64-qt5-* packages

--- devel/generator/gen-kde-fork-revisions.sh
+++ devel/generator/gen-kde-fork-revisions.sh
@@ -4,12 +4,27 @@ shopt -s nullglob
 bindir=$(dirname "$0")
 source "$bindir/../versions.sh"
 expected_version=${versions[qt5]}.*
+echo "expected version: $expected_version"
 
+# determine relevant repositories
+pushd "$bindir/../.."
+repos=()
+for pkgbuild in qt5-*/mingw-w64/PKGBUILD ; do
+    repos+=("${pkgbuild%/mingw-w64/PKGBUILD}")
+done
+popd
+
+# change into the specified directory containing Arch PKGBUILD repos 
 arch_pkgbuilds=$1
 [[ $arch_pkgbuilds ]] && cd "$arch_pkgbuilds"
 
-echo "expected version: $expected_version"
 
+# ensure all relevant repos are cloned
+for repo in "${repos[@]}"; do
+    [[ -d $repo ]] ||[[ $repo == qt5-activeqt ]] || [[ $repo == qt5-winextras ]] ||  [[ $repo == qt5-canvas3d ]] || pkgctl repo clone "$repo"
+done
+
+# ensure all relevant repos are up-to-date
 for pkgbuild in qt5-*/PKGBUILD ; do
     git -C "${pkgbuild%/PKGBUILD}" pull --rebase origin main
 done

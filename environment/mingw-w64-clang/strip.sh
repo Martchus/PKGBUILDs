056edc1d9709e32505917022b27190079ee9ec48
Martchus <martchus@gmx.net>
2024-11-03 23:56:36 +0100

    Fix `mingw-llvm-strip` when only import libs have been specified

--- environment/mingw-w64-clang/strip.sh
+++ environment/mingw-w64-clang/strip.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
 args=()
+file_count=0
 for arg in "$@"; do
     # filter out import libraries because llvm-strip returns "unsupported object file format" for those
-    [[ $arg == *.dll.a ]] || args+=("$arg")
+    [[ $arg == *.dll.a ]] && continue
+    [[ $arg != -* ]] && file_count=$((file_count + 1))
+    args+=("$arg")
 done
-llvm-strip "${args[@]}"
+if [[ $file_count -gt 0 ]]; then
+    llvm-strip "${args[@]}"
+fi

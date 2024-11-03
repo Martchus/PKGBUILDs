8452ddfcdcb12d7625c018e4ae98270798aaccc9
Martchus <martchus@gmx.net>
2024-11-04 00:22:31 +0100

    Hack around libtool library check preventing mingw-w64-clang-aarch64 builds
    This annoying check is not only present in libpng but also libdbus so move
    the hack into the configure wrapper.

--- configure/mingw-w64-clang-aarch64/mingw-configure.sh
+++ configure/mingw-w64-clang-aarch64/mingw-configure.sh
@@ -10,6 +10,11 @@ else
   config_path=".."
 fi
 
+# let check for recognizing dependent libraries always pass
+# note: This is logged as "checking how to recognize dependent libraries... file_magic ^x86 archive import|^x86 DLL" and
+#       obviously not going to work when targeting e.g. aarch64.
+sed -i -e 's|file_magic.*DLL|pass_all|g' ${config_path}/configure
+
 ${config_path}/configure \
   --host=@TRIPLE@ --target=@TRIPLE@ --build="$CHOST" \
   --prefix=/usr/@TRIPLE@ --libdir=/usr/@TRIPLE@/lib --includedir=/usr/@TRIPLE@/include \

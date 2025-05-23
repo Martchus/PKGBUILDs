00399e65ca8bd6d1a69357d6024662dc2592fb1b
Martchus <martchus@gmx.net>
2025-05-23 17:13:30 +0200

    Update Qt 5 packages to 5.15.17

--- devel/versions.sh
+++ devel/versions.sh
@@ -13,7 +13,7 @@ declare -A versions=(
     [blackwidowcontrol]=1.2.0
     [dbus-soundrecorder]=1.2.3
     [geocoordinatecalculator]=1.1.3
-    [qt5]=5.15.16
+    [qt5]=5.15.17
     [qt6]=6.9.0
 )
 declare -A github_names=(

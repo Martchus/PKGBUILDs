1dbff3551c70e0808b4b4b3378bec4a2c2fb8c6c
Martchus <martchus@gmx.net>
2025-07-10 21:21:28 +0200

    Avoid using `-Wl,-z,pack-relative-relocs` in static-compat env
    This linker flag enables the DT_RELR relative relocation format which is
    only supported by glibc 2.36 and thus using it breaks compatibility with
    older distributions.
    
    This flag should stay of course in the general build env keeping it in-line
    with the flags used by Arch Linux and only be omitted when building static-
    compat packaging.
    
    See https://sourceware.org/bugzilla/show_bug.cgi?id=27924 for the issue
    about this feature on the glibc bugtracker.

--- environment/static-compat/static-compat-environment.sh
+++ environment/static-compat/static-compat-environment.sh
@@ -7,6 +7,7 @@ export CXX=$static_compat_prefix/bin/g++
 export CFLAGS="$CFLAGS -fPIC"
 export CXXFLAGS="$CXXFLAGS -fPIC"
 export CPPFLAGS="$CPPFLAGS"
+export LDFLAGS=${LDFLAGS/-Wl,-z,pack-relative-relocs/}
 export PKG_CONFIG_PATH=${PKG_CONFIG_PATH:-$static_compat_prefix/usr/lib/pkgconfig:$static_compat_prefix/usr/share/pkgconfig}
 export PKG_CONFIG_SYSROOT_DIR=${PKG_CONFIG_SYSROOT_DIR:-$static_compat_prefix}
 export PKG_CONFIG_LIBDIR=${PKG_CONFIG_LIBDIR:-$static_compat_prefix/lib/pkgconfig:$static_compat_prefix/share/pkgconfig}

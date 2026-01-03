bf8ba941ce0d3caa2fc91130a361087abc2aa298
Martchus <martchus@gmx.net>
2026-01-03 18:51:57 +0100

    Add script to bump pkgrel

---
+++ devel/bump-pkgrel.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/bash
+
+# setup shell environment
+set -e # abort on first error
+shopt -s nullglob
+shopt -s extglob
+export BASHOPTS
+source /usr/share/devtools/lib/util/pkgbuild.sh
+source /usr/share/makepkg/util/message.sh
+[[ $ALL_OFF ]] || colorize
+
+for pkg_dir in "$@"; do
+    if [[ -f $pkg_dir/default/PKGBUILD ]]; then
+        pkg_dir+=/default
+    fi
+    if [[ ! -f $pkg_dir/PKGBUILD ]]; then
+        msg "$pkg_dir/PKGBUILD does not exist"
+        continue
+    fi
+    pushd "$pkg_dir" > /dev/null
+    pkgname= pkgver= pkgrel=
+    source ./PKGBUILD
+    oldpkgrel=$pkgrel
+    newpkgrel=$((oldpkgrel + 1))
+    msg "Updating $pkgname: $pkgver-$oldpkgrel -> $pkgver-$newpkgrel"
+    pkgbuild_set_pkgrel "$((pkgrel + 1))"
+    popd > /dev/null
+done

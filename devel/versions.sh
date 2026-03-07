576d82bce86ce896db640ff91c9312e98962b895
Martchus <martchus@gmx.net>
2026-03-07 11:18:27 +0100

    Update own projects

--- devel/versions.sh
+++ devel/versions.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 declare -A versions=(
-    [syncthingtray]=2.0.9
+    [syncthingtray]=2.0.10
     [c++utilities]=5.33.0
-    [qtutilities]=6.20.0
+    [qtutilities]=6.21.0
     [qtforkawesome]=0.3.2
     [tagparser]=12.5.2
     [tageditor]=3.9.9

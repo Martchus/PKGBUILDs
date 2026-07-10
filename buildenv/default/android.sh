f51fc878176497af17c0ca4b97f4a0e0db28b198
Martchus <martchus@gmx.net>
2026-07-11 00:20:24 +0200

    Fix build of `android-*-ffmpeg` packages

--- buildenv/default/android.sh
+++ buildenv/default/android.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/bash
 
+# This file is no longer included as it leads to ANDROID_MINIMUM_PLATFORM being
+# cleared with `!buildflags` and that option is necessary in android-* packages.
+# So builds are relying on having the variable in `/etc/makepkg.conf`.
+
 [[ -n "$LIBMAKEPKG_BUILDENV_ANDROID_SH" ]] && return
 LIBMAKEPKG_BUILDENV_ANDROID_SH=1
 

b34dfa011a4cde27480a39a007a9b74d84ff0a23
Martchus <martchus@gmx.net>
2026-07-19 12:37:16 +0200

    Fix passing target in mingw-w64-clang compiler wrappers and target symlinks

--- environment/mingw-w64-clang/clang++.sh
+++ environment/mingw-w64-clang/clang++.sh
@@ -2,6 +2,6 @@
 
 # pass important flags via this wrapper for build systems that don't use CXXFLAGS consistently
 _target_from_name=${0%-clang*}
-_target_from_name=${_name##*/}
-_target=${MINGW_W64_CLANG_TARGET:-${_name:-aarch64-w64-mingw32}}
-clang++ -stdlib=libc++ -rtlib=compiler-rt -fuse-ld=lld -target $_target_from_name -Xclang -triple -Xclang $_target "$@"
+_target_from_name=${_target_from_name##*/}
+_target=${MINGW_W64_CLANG_TARGET:-${_target_from_name:-aarch64-w64-mingw32}}
+clang++ -stdlib=libc++ -rtlib=compiler-rt -fuse-ld=lld -target "$_target" -Xclang -triple -Xclang "$_target" "$@"
